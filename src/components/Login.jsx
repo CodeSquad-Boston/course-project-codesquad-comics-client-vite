3a30e777788cde7923171cc6340fde33139c8540
yusufalp <alpyusuf007@gmail.com>
2024-10-04 21:11:41 -0700

    refactor form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,32 +1,30 @@
 const Login = () => {
   return (
-    <div className="container-content">
-      <div className="form-container">
-        <h1>LOGIN PAGE</h1>
-        <form action="#">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            placeholder="johnwick"
-            required
-          />
-          <br />
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Enter a strong password"
-            required
-          />
-          <br />
-          <div className="button-container-center">
-            <input className="button-yellow" type="submit" />
-          </div>
-        </form>
-      </div>
+    <div className="form-container">
+      <h1>LOGIN PAGE</h1>
+      <form action="#">
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="johnwick"
+          required
+        />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter a strong password"
+          required
+        />
+        <br />
+        <div className="button-container-center">
+          <input className="button-yellow" type="submit" />
+        </div>
+      </form>
     </div>
   );
 };
